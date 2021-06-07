b0b2fd9756187d317e0a83bac0e69fc37a091a27
nurjanovb <toxiccaalien00@gmail.com>
2021-06-07 17:49:33 +0600

    Initial commit

---
+++ app/src/main/java/com/example/lesson3/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.lesson3;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+
+
+    String s1[], s2[];
+    int images[] = {  R.drawable.java,
+            R.drawable.python,
+            R.drawable.php,
+            R.drawable.c_plus_plus,
+            R.drawable.javascript,
+            R.drawable.dart,
+            R.drawable.sql,
+            R.drawable.ccc,
+            R.drawable.ruby,
+            R.drawable.swift,
+            R.drawable.kotlin};
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        recyclerView = findViewById(R.id.recyclerView);
+        s1 = getResources().getStringArray(R.array.programming_languges);
+        s2 = getResources().getStringArray(R.array.description);
+
+
+        RecyclerviewAdapter recyclerviewAdapter = new RecyclerviewAdapter(this, s1, s2, images);
+        recyclerView.setAdapter(recyclerviewAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+}
---
+++ app/src/main/java/com/example/lesson3/RecyclerviewAdapter.java
@@ -0,0 +1,73 @@
+package com.example.lesson3;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class RecyclerviewAdapter extends RecyclerView.Adapter<RecyclerviewAdapter.RecyclerviewHolder> {
+
+    String data1[], data2[];
+    int images[];
+    Context context;
+
+    public RecyclerviewAdapter(Context ct, String s1[], String s2[], int img[]) {
+        context = ct;
+        data1 = s1;
+        data2 = s2;
+        images = img;
+    }
+
+
+    @Override
+    public RecyclerviewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+
+
+        View view = inflater.inflate(R.layout.my_row, parent, false);
+        return new RecyclerviewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerviewAdapter.RecyclerviewHolder holder, int position) {
+        holder.myText1.setText(data1[position]);
+        holder.myText2.setText(data2[position]);
+        holder.myImage.setImageResource(images[position]);
+
+
+        holder.mainLayout.setOnClickListener(v -> {
+            Intent intent = new Intent(context, MainActivity2.class);
+            intent.putExtra("data1",data1[position]);
+            intent.putExtra("data2",data2[position]);
+            intent.putExtra("myImage",images[position]);
+            context.startActivity(intent);
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return images.length;
+    }
+
+    public class RecyclerviewHolder extends RecyclerView.ViewHolder {
+        TextView myText1, myText2;
+        ImageView myImage;
+        ConstraintLayout mainLayout;
+
+        public RecyclerviewHolder(View itemView) {
+            super(itemView);
+            myText1 = itemView.findViewById(R.id.txt_title);
+            myText2 = itemView.findViewById(R.id.txt_descriptiopn);
+            myImage = itemView.findViewById(R.id.image_icon);
+            mainLayout = itemView.findViewById(R.id.mainLayout);
+        }
+    }
+}
